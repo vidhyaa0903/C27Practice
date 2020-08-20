06a9add42968dda80725a56ec812c19c94e705c6
vidhya0903 <vidhyaajayj@gmail.com>
2020-08-20 16:17:33 +0530

    add chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+var bird,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +17,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -36,6 +38,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
+  
 }
 
 function draw(){
@@ -61,4 +67,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
+
 }
